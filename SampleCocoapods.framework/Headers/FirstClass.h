9dac1dc441f97447890af7abee1c0ed3ad45da7a
Saurabh Mendhe <saurabh.mendhe@mobiliya.com>
2018-10-12 11:06:24 +0530

    Added framework

---
+++ SampleCocoapods.framework/Headers/FirstClass.h
@@ -0,0 +1,14 @@
+//
+//  FirstClass.h
+//  SampleCocoapods
+//
+//  Created by Saurabh Mendhe on 12/10/18.
+//  Copyright © 2018 Saurabh Mendhe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstClass : NSObject
+
+-(void)firstMethod;
+@end
---
+++ SampleCocoapods.framework/Headers/SampleCocoapods.h
@@ -0,0 +1,19 @@
+//
+//  SampleCocoapods.h
+//  SampleCocoapods
+//
+//  Created by Saurabh Mendhe on 12/10/18.
+//  Copyright © 2018 Saurabh Mendhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleCocoapods.
+FOUNDATION_EXPORT double SampleCocoapodsVersionNumber;
+
+//! Project version string for SampleCocoapods.
+FOUNDATION_EXPORT const unsigned char SampleCocoapodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleCocoapods/PublicHeader.h>
+
+#import "FirstClass.h"
